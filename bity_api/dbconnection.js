25e943c1b6d22468578392594d1cb7ed8c09ac9b
Patel, Ashish <AshishM_Patel@spe.sony.com>
2018-01-18 10:19:22 -0800

    t1

--- bity_api/dbconnection.js
+++ bity_api/dbconnection.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var UserAuthDB = require('./UserAuthenticationDB.js');
+var UserAuthDB = require('./dal/UserAuthenticationDB.js');
 var UserLoginDB = require('./dal/UserLoginDB');
 var UserProfileDB = require('./dal/UserProfileDB');
 var ProductDB = require('./dal/ProductDB');
